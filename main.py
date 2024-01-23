099ce7001bed9d2641823b25b7e70549516906c9
4YR <54866157+4YR@users.noreply.github.com>
2024-01-23 14:11:47 +0400

    Start new project

---
+++ main.py
@@ -0,0 +1,10 @@
+from bs4 import BeautifulSoup
+import requests
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
+}
+
+url = 'https://www.cbr.ru/key-indicators/'
+response = requests.get(url, headers=headers)
+soup = BeautifulSoup(response.text, 'html.parser')
