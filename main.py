8d26a3c30374c1157cd4d729d05d2a1bcc9690aa
4YR <54866157+4YR@users.noreply.github.com>
2024-01-24 14:01:52 +0400

    add name currency

--- main.py
+++ main.py
@@ -10,7 +10,16 @@ response = requests.get(url, headers=headers)
 soup = BeautifulSoup(response.text, 'html.parser')
 
 currency = soup.find_all("div", class_="d-flex title-subinfo")
+price = soup.find_all("td", class_="value td-w-4 _bold _end mono-num")
 
-for i in currency:
-    name = i.find("div", class_="col-md-5").text.strip()
-    print(name)
+
+
+
+for i in range(len(currency)):
+    name = currency[i].find("div", class_="col-md-5").text.strip()
+    price_el = price[i].text.strip()
+
+    print(f'{name}: {price_el}')
+
+
+
